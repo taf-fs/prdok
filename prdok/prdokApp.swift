5a732fc5e816f1c16c5e9d76d987878ee0e71fe2
taf-fs <53691963+taf-fs@users.noreply.github.com>
2025-10-12 14:39:07 +0200

    Initial Commit

---
+++ prdok/prdokApp.swift
@@ -0,0 +1,17 @@
+//
+//  prdokApp.swift
+//  prdok
+//
+//  Created by David Horňák on 12.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct prdokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
