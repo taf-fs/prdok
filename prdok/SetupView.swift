c1fea3869139a3192938efe15c5b71b212e59680
taf-fs <53691963+taf-fs@users.noreply.github.com>
2025-10-18 22:06:37 +0200

    SetupView

---
+++ prdok/SetupView.swift
@@ -0,0 +1,81 @@
+//
+//  SetupView.swift
+//  prdok
+//
+//  Created by David Horňák on 18.10.2025.
+//
+
+import SwiftUI
+
+struct SetupView: View {
+    @Environment(\.colorScheme) var colorScheme
+    var isDarkMode: Bool {
+        return colorScheme == .dark
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Vítej v aplikaci")
+                .font(.system(.subheadline))
+                .foregroundStyle(.secondary)
+            
+            Text("Prdok_")
+                .font(.system(.largeTitle, design: .monospaced))
+                .fontWeight(.semibold)
+            
+            Spacer()
+            
+            Image(isDarkMode ? "cpLogoDark" : "cpLogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: 150, maxHeight: 150)
+                .opacity(0.9)
+            
+            Spacer()
+            
+            Text("K získání přístupu do aplikace, je třeba spárovat aplikaci se zaměstnaneckým webem.")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+            
+            Button {
+                // TODO: logic
+            } label: {
+                Text("Spárovat přes QR kód")
+                    .bold()
+                    .padding(.vertical, 8)
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.background)
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(.primary)
+                            .tint(.primary)
+                    }
+            }
+            
+            Button {
+                // TODO: logic
+            } label: {
+                Text("Spárovat přes zaměstnanecký odkaz")
+                    .bold()
+                    .padding(.vertical, 8)
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.secondary)
+                    .tint(.primary)
+//                    .background {
+//                        RoundedRectangle(cornerRadius: 16)
+//                            .stroke(.secondary, lineWidth: 1)
+//                            .tint(.primary)
+//                    }
+
+            }
+        }
+        .padding(.horizontal)
+        .padding(.bottom, 32)
+        .padding(.top, 128)
+    }
+}
+
+#Preview {
+    SetupView()
+}
--- prdok/prdokApp.swift
+++ prdok/prdokApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct prdokApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            SetupView()
         }
     }
 }
