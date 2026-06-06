1df53b9b43876402fe7f2746a64a7c664693e3b7
taf-fs <53691963+taf-fs@users.noreply.github.com>
2026-06-06 23:15:56 +0200

    add and move dev credentials to separate .plist

--- prdok/prdokApp.swift
+++ prdok/prdokApp.swift
@@ -10,16 +10,34 @@ import SwiftUI
 @main
 struct prdokApp: App {
     init() {
+        #if DEBUG
+        loadDevCredentials()
+        #endif
+
         if UserDefaults.standard.bool(forKey: "setupCompleted") == false {
             UserDefaults.standard.set(false, forKey: "needsToBootstrap")
         } else {
             UserDefaults.standard.set(true, forKey: "needsToBootstrap")
         }
     }
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+#if DEBUG
+private func loadDevCredentials() {
+    guard let url = Bundle.main.url(forResource: "DevCredentials", withExtension: "plist"),
+          let dict = NSDictionary(contentsOf: url) as? [String: String] else {
+        fatalError("DevCredentials.plist missing. Create DevCredentials.plist and fill in your values.")
+    }
+    UserDefaults.standard.set(true, forKey: "setupCompleted")
+    UserDefaults.standard.set(dict["klic"],     forKey: "klic")
+    UserDefaults.standard.set(dict["id"],       forKey: "id")
+    UserDefaults.standard.set(dict["ids"],      forKey: "ids")
+    UserDefaults.standard.set(dict["skladnik"], forKey: "skladnik")
+}
+#endif
